b6b65e4b213f40a83d8746099f11a911f467d28f
Abdullahi <abdullahi45230@student.simad.edu.so>
2026-08-01 20:13:57 +0300

    Conditional with &&

--- src/index.js
+++ src/index.js
@@ -67,15 +67,20 @@ function Header() {
   );
 }
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaOb={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaOb={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
 
       {/* <Pizza
         name="Pizza Spinaci"
@@ -120,8 +125,12 @@ function Footer() {
   //   );
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We are currently{" "}
-      {isOpen ? "open" : "closed"}{" "}
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00 Come visit us or order online. </p>
+          <button className="btn">Order Now</button>
+        </div>
+      )}
     </footer>
   );
 }
