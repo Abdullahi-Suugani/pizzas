f28cf767b22ed501fbab08e79a05019dadb48233
Abdullahi <abdullahi45230@student.simad.edu.so>
2026-08-02 19:33:27 +0300

    JSX Into a New Component

--- src/index.js
+++ src/index.js
@@ -102,9 +102,7 @@ function Menu() {
 }
 
 function Pizza(props) {
-
   if (props.pizzaOb.soldOut) return null;
-   
 
   return (
     <li className="pizza">
@@ -140,16 +138,25 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00 Come visit us or order online. </p>
-          <button className="btn">Order Now</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           we're happy to welcome you between {openHour}:00 and {closeHour}:00
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00 Come visit us or order
+        online.{" "}
+      </p>
+      <button className="btn">Order Now</button>
+    </div>
   );
 }
 
